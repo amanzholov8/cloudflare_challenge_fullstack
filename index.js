9f498ea6ab39a56a9c931efa40dd5ca2d2166e24
Alim Amanzholov <alim.amanzholov@gmail.com>
2020-04-23 13:46:22 +0900

    Implement main part of challenge

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
+
 /**
  * Respond with hello worker text
  * @param {Request} request
  */
-async function handleRequest(request) {
-  return new Response('Hello worker!', {
-    headers: { 'content-type': 'text/plain' },
-  })
+handleRequest = async (request) => {
+  const variants = await getVariantsFromAPI();
+
+  const randomIndex = Math.floor(Math.random() * variants.length);
+  const variantPage = await getVariantPage(variants, randomIndex);
+
+  return variantPage;
+}
+
+getVariantsFromAPI = async () => {
+  const response = await fetch('https://cfw-takehome.developers.workers.dev/api/variants');
+  const responseJSON = await response.json();
+  
+  return responseJSON.variants;
+}
+
+getVariantPage = async (variants, randomIndex) => {
+  const variant = variants[randomIndex];
+  
+  const response = await fetch(variant);
+  return response;
 }
